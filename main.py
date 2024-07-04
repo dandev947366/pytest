746617117f5007675f8c1f24fe866880a54e127b
danofoundation <dan@ofoundation.nl>
2024-07-04 08:11:22 +0300

    add rectangle tests

--- main.py
+++ main.py
@@ -28,3 +28,9 @@ class Rectangle(Shape):
     def __init__(self, length, width):
         self.length = length
         self.width = width
+        
+    def area(self):
+        return self.length * self.width
+    
+    def perimeter(self):
+        return (self.length*2) + (self.width*2)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import pytest
-from main import add, divide, Circle
+from main import add, divide, Circle, Rectangle
 import math
 def test_add():
     result = add(1, 4)
@@ -29,3 +29,18 @@ class TestCircle:
         result = 2 * math.pi * self.circle.radius
         assert self.circle.perimeter() == result
         
+def test_rec_area():
+    rectangle = Rectangle(10, 20)
+    assert rectangle.area() == 10 * 20
+
+def test_rec_perimeter():
+    rectangle = Rectangle(10, 20)
+    assert rectangle.perimeter() == (10*2) + (20*2)
+    
+def test_circle_area():
+    circle = Circle(10)
+    assert circle.area() == math.pi * 10 ** 2
+    
+def test_circle_perimeter():
+    circle = Circle(10)
+    assert circle.perimeter() == 2 * math.pi * 10
