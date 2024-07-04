82249ebf5b75a989000a77df07059a16667783c2
danofoundation <dan@ofoundation.nl>
2024-07-04 08:22:43 +0300

    add __eq__

--- test.py
+++ test.py
@@ -32,18 +32,26 @@ class TestCircle:
 @pytest.fixture
 def my_rectangle():
     return Rectangle(10, 20)
-
+    
+@pytest.fixture
+def my_weird_rectangle():
+    return Rectangle(5, 6)
+    
 def test_rec_area(my_rectangle):
     assert my_rectangle.area() == 10 * 20
 
 def test_rec_perimeter(my_rectangle):
     assert my_rectangle.perimeter() == (10*2) + (20*2)
     
+def test_not_equal(my_rectangle, my_weird_rectangle):
+    assert my_rectangle != my_weird_rectangle
+    
 @pytest.fixture
 def my_circle():
     return Circle(10)
+    
 def test_circle_area(my_circle):
     assert my_circle.area() == math.pi * 10 ** 2
-    
+
 def test_circle_perimeter(my_circle):
     assert my_circle.perimeter() == 2 * math.pi * 10
