72b2b40a5528a6d5df6b768a444e5dcba77534b8
danofoundation <dan@ofoundation.nl>
2024-07-04 08:01:10 +0300

    add cirlce tests

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 def add(num_one, num_two):
     return num_one + num_two
     
@@ -5,3 +7,24 @@ def divide(num_one, num_two):
     if num_two == 0:
         raise ValueError("Division by zero is not allowed.")
     return num_one/num_two
+    
+class Shape:
+    def area(self):
+        pass
+    def perimeter(self):
+        pass
+        
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self): 
+        return math.pi * self.radius ** 2
+    
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+        
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import pytest
-from main import add, divide
-
+from main import add, divide, Circle
+import math
 def test_add():
     result = add(1, 4)
     assert result == 5
+    
+def test_add_string():
+    result = add('i like ', 'burgers')
+    assert result == 'i like burgers'
     
 def test_divide():
     result = divide(10, 5)
@@ -12,3 +16,16 @@ def test_divide():
 def test_divide_by_zero():
     with pytest.raises(ValueError):
         divide(10, 0)
+        
+class TestCircle:
+    def setup_method(self, method):
+        self.circle = Circle(10)
+        
+    def test_area(self):
+        result = math.pi * self.circle.radius ** 2
+        assert self.circle.area() == result
+        
+    def test_perimeter(self):
+        result = 2 * math.pi * self.circle.radius
+        assert self.circle.perimeter() == result
+        
